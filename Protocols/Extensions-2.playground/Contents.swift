7b149f03f285188a7930e155cd5c9d656531243b
Christopher Hicks <visrule@protonmail.com>
2022-04-16 18:24:03 -0500

    protocols extensions

--- Protocols/Extensions-2.playground/Contents.swift
+++ Protocols/Extensions-2.playground/Contents.swift
@@ -1,40 +1,58 @@
 import UIKit
 
+
+
 protocol Account {
-    
     var balance: Double { get set }
-    
     mutating func deposit(_ amount: Double)
     mutating func withdraw(_ amount: Double)
     func calculateInterestEarned() -> Double
     func transfer(from: Account, to: Account, amount: Double)
-    
 }
 
-// Become default implementation
 extension Account {
     mutating func deposit(_ amount: Double) { balance += amount }
     mutating func withdraw(_ amount: Double) { balance -= amount }
     func calculateInterestEarned() -> Double { return (balance * (0.1/100)) }
 }
 
-struct CheckingAccount: Account {
-    var balance: Double
-    
-    func transfer(from: Account, to: Account, amount: Double) {
-        //
+struct VerificationRequest {
+    let accounts: [Account]
+}
+
+struct VerificationResponse {
+    let verified: Bool
+}
+
+// 2nd Protocol
+protocol Verification {
+    func performVerification(_ request: VerificationRequest, complete: (VerificationResponse) -> Void)
+}
+
+extension Verification {
+    func performVerification(_ request: VerificationRequest, complete: (VerificationResponse) -> Void) {
+        // perform verificaton logic
     }
-    
-    func calculateInterestEarned() -> Double { return (balance * (0.2/100)) }
 }
 
 
-struct MoneyMarketAccount: Account {
+struct CheckingAccount: Account, Verification {
+    var balance: Double
+    
+    func transfer(from: Account, to: Account, amount: Double) {
+        self.performVerification(VerificationRequest(accounts: [from, to])) { (response) in
+            if response.verified {
+                // transfer funds
+            }
+        }
+    }
+}
+
+
+struct MoneyMarketAccount: Account, Verification {
     var balance: Double
     
     func transfer(from: Account, to: Account, amount: Double) {
         //
     }
-    
-    func calculateInterestEarned() -> Double { return (balance * (0.4/100)) }
 }
--- README.md
+++ README.md
@@ -1368,8 +1368,6 @@ func runParser<P: Parser>(parser: P, input: [P.Input]) where P.Input == JsonPars
 
 ##### part one 
 
-*extensions can run as a default implementation*
-
 ```swift
 protocol Parser {
     associatedtype Input
@@ -1439,5 +1437,55 @@ struct MoneyMarketAccount: Account {
     }
     
     func calculateInterestEarned() -> Double { return (balance * (0.4/100)) }
+}
+```
+
+##### part three
+
+```swift
+protocol Account {
+    var balance: Double { get set }
+    mutating func deposit(_ amount: Double)
+    mutating func withdraw(_ amount: Double)
+    func calculateInterestEarned() -> Double
+    func transfer(from: Account, to: Account, amount: Double)
+}
+
+extension Account {
+    mutating func deposit(_ amount: Double) { balance += amount }
+    mutating func withdraw(_ amount: Double) { balance -= amount }
+    func calculateInterestEarned() -> Double { return (balance * (0.1/100)) }
+}
+
+struct VerificationRequest {
+    let accounts: [Account]
+}
+
+struct VerificationResponse {
+    let verified: Bool
+}
+
+// 2nd Protocol
+protocol Verification {
+    func performVerification(_ request: VerificationRequest, complete: (VerificationResponse) -> Void)
+}
+
+extension Verification {
+    func performVerification(_ request: VerificationRequest, complete: (VerificationResponse) -> Void) {
+        // perform verificaton logic
+    }
+}
+
+
+struct CheckingAccount: Account, Verification {
+    var balance: Double
+    
+    func transfer(from: Account, to: Account, amount: Double) {
+        self.performVerification(VerificationRequest(accounts: [from, to])) { (response) in
+            if response.verified {
+                // transfer funds
+            }
+        }
+    }
 }
 ```
