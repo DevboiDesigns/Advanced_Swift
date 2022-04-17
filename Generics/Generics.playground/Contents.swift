3d40bc1f87fcfebd947d1b3245dfacd8fd1da52f
Christopher Hicks <visrule@protonmail.com>
2022-04-16 19:02:46 -0500

    generics usage

--- Generics/Generics.playground/Contents.swift
+++ Generics/Generics.playground/Contents.swift
@@ -1,3 +1,29 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Movie {
+    let name: String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+let names = ["Alex", "John", "Mary", "Steve"]
+let movies = [Movie("Batman"), Movie("Spiderman"), Movie("Superman")]
+
+// <T> T = can be any name
+func firstLast<T>(_ list: [T]) -> (T, T) {
+    return (list[0], list[list.count - 1])
+}
+
+let (first, last) = firstLast(numbers)
+let (nameFirst, nameLast) = firstLast(names)
+let (firstMovie, lastMovie) = firstLast(movies)
+
+print(first)
+print(last)
+print(nameFirst)
+print(nameLast)
+print(firstMovie)
+print(lastMovie)
--- README.md
+++ README.md
@@ -1594,5 +1594,32 @@ struct InternationalStudent: Student, VerifyStudent {
 ### usage
 
 ```swift
+struct Movie {
+    let name: String
+    
+    init(_ name: String) {
+        self.name = name
+    }
+}
+
+let numbers = [1,2,3,4,5,6,7,8,9]
+let names = ["Alex", "John", "Mary", "Steve"]
+let movies = [Movie("Batman"), Movie("Spiderman"), Movie("Superman")]
+```
+
+ `<T>`  **T = can be any name**
+```swift
+func firstLast<T>(_ list: [T]) -> (T, T) {
+    return (list[0], list[list.count - 1])
+}
+
+let (first, last) = firstLast(numbers)
+let (nameFirst, nameLast) = firstLast(names)
+let (firstMovie, lastMovie) = firstLast(movies)
+```
+
+### constraints
+
+```swift
 
 ```
